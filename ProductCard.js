0d7fc06ee4e65221b753cf6588086b100278b641
zakkarias1701 <t-silva-pereira@bol.com.br>
2025-12-03 16:46:43 -0300

    Delete ProductCard.js

--- ProductCard.js
+++
@@ -1,8 +0,0 @@
-export default function ProductCard({title,price,link}){
-  return (
-    <a href={link} style={{border:"1px solid #ccc",padding:10,borderRadius:8,textDecoration:"none",color:"#000"}}>
-      <div>{title}</div>
-      <strong>{price}</strong>
-    </a>
-  );
-}
